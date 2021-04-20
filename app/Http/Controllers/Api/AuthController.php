2fe0c1b96e75847659dded8c7f2723a0daabc28b
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-20 11:05:21 +0700

    Add trait ApiResponse in AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    use ApiResponse;
 }
