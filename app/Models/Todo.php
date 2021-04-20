109524c0fad2b1afabe55c9897959169c0991afc
Syaifuddin Zuhri <mochammadsyaifuddinzuhri@gmail.com>
2021-04-20 11:58:19 +0700

    Edit model Todo

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'todo',
+        'label',
+        'done'
+    ];
+
+    protected $hidden = ['user_id'];
+
+    protected $casts = ['done'];
+
+    /**
+     * Get the user that owns the Todo
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
